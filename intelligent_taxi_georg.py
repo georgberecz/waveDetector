3d829572418a40dd99a3f634208ff6dc792333ee
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2015-11-20 12:33:02 +0200

    try faster face detection for human recognition

---
+++ intelligent_taxi_georg.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+
+
+videoFileName="VIDEO0060.mp4"
+capture = cv2.VideoCapture(videoFileName)
+
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface.xml')
+i = 0;
+factor = 0.5
+de_factor = 1 / factor
+while capture.get(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO) < 1:
+	# Capture frame-by-frame
+	ret,frame=capture.read()
+	if i % 2 == 0:
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		resized_gray = cv2.resize(gray, (0,0), fx = factor, fy = factor)
+		rects = faceCascade.detectMultiScale(resized_gray)
+	#show the frame
+	for rect in rects:
+		cv2.rectangle(frame,(int(rect[0] * de_factor), int(rect[1] * de_factor)),(int(rect[0] * de_factor + rect[2] * de_factor), int(rect[1] * de_factor + rect[3] * de_factor)),(255,0,0),1)
+	cv2.imshow('frame',frame)
+	i += 1
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+
+
+# When everything done, release the capture
+capture.release()
+cv2.destroyAllWindows()
